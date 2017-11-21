b335fc3befe2c11653849cd85efce1237ab5c3fd
Calvin Ruben <calvin@Ashley-Rubens-MacBook-Pro.local>
2017-11-20 18:44:54 -0700

    Layout setup

---
+++ UTARider/FavoritesView.swift
@@ -0,0 +1,15 @@
+//
+//  FavoritesView.swift
+//  UTARider
+//
+//  Created by Calvin Ruben on 11/20/17.
+//  Copyright © 2017 Rubenmobile. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesView : UITableViewController {
+    
+    
+    
+}
---
+++ UTARider/FlyOutMenu.swift
@@ -0,0 +1,13 @@
+//
+//  FlyOutMenu.swift
+//  UTARider
+//
+//  Created by Calvin Ruben on 11/20/17.
+//  Copyright © 2017 Rubenmobile. All rights reserved.
+//
+
+import UIKit
+
+class FlyOutMenu: UIViewController {
+    
+}
